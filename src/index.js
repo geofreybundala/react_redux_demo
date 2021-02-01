a70c1d4b5d633b8076faa585d120732ed72bd2ce
geofreybundala <bundalageofrey2@gmail.com>
2021-02-01 12:47:53 +0300

    feat(set redux)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './components/App'
+import App from './components/App';
+import {Provider} from 'react-redux'
+import {creatStore} from 'redux'
+import reducers from './reducers/'
 
 
 ReactDOM.render(
-    <App/>,
+    <provider store={creatStore(reducers)}>
+        <App/>
+        </provider>
+    
+    ,
     document.querySelector('#root')
 )
---
+++ src/reducers/index.js
@@ -0,0 +1,26 @@
+import {combinedReducers} from 'redux';
+
+const songsReducers =()=>{
+    return [
+        {title:'No SCrups', duration:'4:04'},
+        {title:'Macarena', duration:'3:27'},
+        {title:'No air', duration:'2:04'},
+        {title:'I wanted it that way', duration:'3:15'}
+    ]
+}
+
+
+const selectedSongReducer =(selectedSong=null,action)=>{
+    if(action.type==='SONG_SELECTED'){
+        return action.payload
+    }
+
+    return selectedSong
+}
+
+export default combinedReducers({
+    songs:songsReducers,
+    selectedSong:selectedSongReducer
+})
+
+
